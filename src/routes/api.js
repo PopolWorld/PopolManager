721652d7fe4a0e0c9a2e0281ec0bab3b7b7bbd6b
Nathan Fallet <contact@nathanfallet.me>
2020-10-25 15:28:20 +0100

    PUT /player/:uuid

--- src/routes/api.js
+++ src/routes/api.js
@@ -12,6 +12,10 @@ const server = require('../models/server');
 const player = require('../models/player');
 const minecraft = require('../models/minecraft');
 
+/**
+ * Middlewares
+ */
+
 // Middleware to get player by uuid
 async function getPlayerByUUID(req, res, next) {
     // Get player from database
@@ -20,7 +24,7 @@ async function getPlayerByUUID(req, res, next) {
     });
 
     // Check if player was found
-    if (found == undefined) {
+    if (found === null) {
         // If not, return with a 404
         return res.status(404).json({ message: 'Player not found' });
     }
@@ -38,7 +42,7 @@ async function getServerByID(req, res, next) {
     });
 
     // Check if player was found
-    if (found == undefined) {
+    if (found === null) {
         // If not, return with a 404
         return res.status(404).json({ message: 'Server not found' });
     }
@@ -52,13 +56,18 @@ async function getServerByID(req, res, next) {
 
 // Middleware to check server token
 async function checkToken(req, res, next) {
+    // Check token is specified and not empty
+    if (req.header('token') === undefined || req.header('token') === '') {
+        return res.status(403).json({ message: 'Access denied' });
+    }
+
     // Get server from database
     const found = await server.findOne({
         where: { token: req.header('token') }
     });
 
     // Check if server was found
-    if (found == undefined) {
+    if (found === null) {
         // If not, return with a 403
         return res.status(403).json({ message: 'Access denied' });
     }
@@ -71,6 +80,10 @@ async function checkToken(req, res, next) {
     // Continue
     next();
 }
+
+/**
+ * Servers
+ */
 
 // Get all servers
 router.get('/server/', async (req, res) => {
@@ -100,9 +113,46 @@ router.put('/server/:id', getServerByID, checkToken, async (req, res) => {
     res.json(res.server);
 });
 
+/**
+ * Player
+ */
+
 // Get player by uuid
 router.get('/player/:uuid', getPlayerByUUID, async (req, res) => {
     res.json(res.player);
+});
+
+// Update player by id
+router.put('/player/:uuid', checkToken, async (req, res) => {
+    // Get player from database
+    let found = await player.findOne({
+        where: { uuid: req.params.uuid }
+    });
+
+    // Check if player exists
+    if (found !== null) {
+        // Update it
+        if (req.body.name !== undefined) {
+            found.name = req.body.name;
+        }
+        await found.save({ fields: ['name'] });
+    } else if (req.body.name !== undefined) {
+        // Create it
+        found = await player.create({
+            uuid: req.params.uuid.toLowerCase(),
+            name: req.body.name,
+            level: '' // TODO: Get default when level system is done
+        });
+
+        // Change HTTP status to 201 Created
+        res.status(201);
+    } else {
+        // Missing arguments
+        return res.status(400).json({ message: 'Missing player name!' });
+    }
+
+    // Return new response
+    res.json(found);
 });
 
 // Export
