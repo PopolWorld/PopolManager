5a3e76a389002ad821fec1114e1ccdd454192757
Nathan Fallet <contact@nathanfallet.me>
2020-10-30 13:26:37 +0100

    Setting up frontend with vuejs

--- src/routes/www/index.js
+++ src/routes/www/index.js
@@ -1,13 +1,21 @@
 // Import modules
 const express = require('express');
+const expressVue = require('express-vue');
+const path = require('path');
 
 // Create a router
 const router = express.Router();
 
-// Test home page
-router.get('/', async (req, res) => {
-    res.send('Hello popolworld!');
-})
+// Register vuejs
+router.use(expressVue.init({
+    rootPath: path.join(__dirname, './')
+}));
+
+// Register views
+router.get('/', async (req, res) => res.renderVue('home.vue'));
+
+// Custom redirections
+router.get('/discord', async (req, res) => res.redirect(301, 'https://discord.gg/gvpsnrc6D3'));
 
 // Export
 module.exports = router;
