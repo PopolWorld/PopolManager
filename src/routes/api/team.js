0da040117f9b87f97636553a92750f6e4d03ffa1
Nathan Fallet <contact@nathanfallet.me>
2020-11-24 23:29:07 +0100

    Change team member role

--- src/routes/api/team.js
+++ src/routes/api/team.js
@@ -7,6 +7,7 @@ const router = express.Router();
 // Get models
 const team = require('../../models/team');
 const player = require('../../models/player');
+const team_member = require('../../models/team_member');
 const { checkToken, getPlayerByUUID, getTeamByID } = require('./utils');
 
 // Get team leaderboard
@@ -113,6 +114,33 @@ router.post('/:id/:uuid', checkToken, getTeamByID, getPlayerByUUID, async (req, 
 router.delete('/:id/:uuid', checkToken, getTeamByID, getPlayerByUUID, async (req, res) => {
     // Remove player from team
     await res.team.removePlayer(res.player);
+
+    // Return new response
+    await res.team.reload();
+    res.json(res.team);
+});
+
+// Edit team member
+router.put('/:id/:uuid', checkToken, getTeamByID, getPlayerByUUID, async (req, res) => {
+    // Get team member
+    const found = await team_member.findOne({
+        where: {
+            teamId: res.team.id,
+            playerUuid: res.player.uuid
+        }
+    });
+
+    // Check if team member was found
+    if (found === null) {
+        // If not, return with a 404
+        return res.status(404).json({ message: 'This player is not in this team!' });
+    }
+
+    // Update values
+    if (req.body.role !== undefined) {
+        found.role = req.body.role;
+    }
+    await found.save({ fields: ['role'] });
 
     // Return new response
     await res.team.reload();
